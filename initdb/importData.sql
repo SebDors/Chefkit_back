3d5a281f834deb75dc3d00ba4d9e654ed74b6963
camille <camille.gravier@epfedu.fr>
2025-10-07 14:46:52 +0200

    importdata fichier

---
+++ initdb/importData.sql
@@ -0,0 +1,25 @@
+INSERT INTO
+    ingredients (nom_ingredient)
+VALUES ('Tomato'),
+    ('Onion'),
+    ('Garlic'),
+    ('Chicken Breast'),
+    ('Olive Oil'),
+    ('Salt'),
+    ('Black Pepper'),
+    ('Basil'),
+    ('Oregano'),
+    ('Parmesan Cheese');
+
+INSERT INTO
+    users (username, email, password)
+VALUES (
+        'seb',
+        'dorssebastien',
+        'password'
+    ),
+    (
+        'admin',
+        'admin@admin',
+        'admin'
+    );
